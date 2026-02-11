6e20ecb6dc54232641156296a3df52ccc1eaadf1
hannah-becker <becker1123@proton.me>
2026-02-11 15:34:05 +0100

    set tile dimensions

--- tiling.R
+++ tiling.R
@@ -17,7 +17,7 @@ index <- read.csv("./aux-files/tile_index_heathlands.csv")[[1]]
     if(basename(files[j]) %in% index){
     img <- rast(files[j])
     dim <- dim(img)[1:2] #check dimension, excluding channel count
-    tileGrid <- c(ceiling(dim[1]/20000), ceiling(dim[2]/20000)) #check how many tiles no larger than 20000px in either direction we can get (to do: check maximum feasible tile size on other laptops as well)
+    tileGrid <- c(ceiling(dim[1]/4096), ceiling(dim[2]/4096)) #check how many tiles no larger than 20000px in either direction we can get (to do: check maximum feasible tile size on other laptops as well)
     tileExtent <- ceiling(dim/tileGrid)
     filename <- gsub("DBU_labeling", "DBU_tiles", gsub(".tif", "_tile_.tif", files[j]))
     makeTiles(img, tileExtent, na.rm = TRUE, extend = FALSE, filename)
@@ -25,5 +25,5 @@ index <- read.csv("./aux-files/tile_index_heathlands.csv")[[1]]
   }
 }
 #)
-
+#8192
 #files are automatically LZW compressed. gdal="COMPRESS=NONE" to turn off
